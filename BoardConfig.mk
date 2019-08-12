d048d57f083af241e77769bba50e8104f99bfb5e
Bauuuuu <abansode@outlook.com>
2019-08-12 12:24:04 +0200

    land: dont use SDCLANG

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,8 +45,7 @@ BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE :=  2048
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x01000000 --second_offset 0x00f00000 --tags_offset 0x00000100
-TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
-
+#TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := land_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8937
 
@@ -208,7 +207,6 @@ TARGET_PER_MGR_ENABLED := true
 # Qualcomm
 BOARD_USES_QCOM_HARDWARE := true
 BOARD_USES_QC_TIME_SERVICES := true
-TARGET_USE_SDCLANG := true
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
